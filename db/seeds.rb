999efb61c3e87faba4f58aed2df6b69b568e0511
Katherine Wise <k06kw02@gmail.com>
2021-08-19 16:45:34 +0100

    created part of the movies app with a nifty api

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,27 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+require "open-uri"
+require "json"
+puts "Cleaning database..."
+Movie.destroy_all
+
+puts "Creating restaurants..."
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+url = "http://tmdb.lewagon.com/movie/top_rated"
+movie_data = JSON.parse(open(url).read)["results"]
+
+movie_data.each do |movie|
+
+  params = {
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie["backdrop_path"]}",
+    rating: movie["vote_average"]
+  }
+  new_movie = Movie.create(params)
+  puts new_movie.id
+end
+
+#
